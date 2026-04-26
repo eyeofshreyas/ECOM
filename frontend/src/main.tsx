a404f75351fd5922151374b87303e26b89eae6e3
shreyas <shreshshende.777@gmail.com>
2026-04-26 10:56:46 +0530

    feat: wrap app with GoogleOAuthProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </GoogleOAuthProvider>
 );
